cbe118067d4d5a1c2a5d50782d24ee190d64f382
jusira <alicjagosiewska@gmail.com>
2019-09-17 19:32:13 +0200

    Upload codes

---
+++ Section2_5_breakdown_scenarios.R
@@ -0,0 +1,54 @@
+library(DALEX2)
+library(iBreakDown)
+library(reticulate)
+
+set.seed(123)
+
+titanic_train <- read.csv("titanic_bd.csv")
+head(titanic_train)
+
+X_train <- titanic_train[,c(3:5)]
+Y_train <- titanic_train$Survived
+
+rf <- py_load_object("rf_classifier.pkl", pickle = "pickle")
+
+predict_function <- function(model, newdata){
+  model$predict_proba(newdata)[,2]
+}
+predict_function(rf, X_train[274, ])
+
+
+# iBreakDown
+rf_explain1 <- explain(rf, data = X_train,
+                      y = Y_train, label = "Scenario 1",
+                      predict_function = predict_function)
+rf_explain2 <- explain(rf, data = X_train,
+                       y = Y_train, label = "Scenario 2",
+                       predict_function = predict_function)
+rf_explain3 <- explain(rf, data = X_train,
+                       y = Y_train, label = "Scenario 3",
+                       predict_function = predict_function)
+
+i <- 274
+passanger <- X_train[i,]
+(exp_1 <- local_attributions(rf_explain1, X_train[274,], order=c(2, 1,3)))
+p11 <- plot(exp_1)
+
+
+(exp_2 <- local_attributions(rf_explain2, X_train[274,], order=c(2, 3,1)))
+p12 <- plot(exp_2)
+
+
+(exp_3 <- local_interactions(rf_explain3, X_train[274,]))
+p13 <- plot(exp_3)
+
+
+library(patchwork)
+library(ggplot2)
+
+(p11 + ggtitle("Scenario 1") )/( p12 + ggtitle("Scenario 2")) # 
+
+p13 + ggtitle("Scenario 3")
+
+
+
---
+++ Section3_explanation_uncertaionty.R
@@ -0,0 +1,30 @@
+library(DALEX2)
+library(iBreakDown)
+library(reticulate)
+library(ranger)
+
+titanic_train <- read.csv("D:\\Alicja\\micro_work\\UAI2019\\titanic_bd.csv")
+head(titanic_train)
+
+titanic <- titanic_train[,-1]
+titanic$Survived <- factor(titanic$Survived)
+
+# baseline random forest
+set.seed(123)
+rf <- ranger(Survived~., titanic, probability = TRUE)
+
+predict_function <- function(m, newdata){
+  predict(m, data = newdata)$predictions[,2]
+}
+predict_function(rf, titanic)
+
+# iBreakDown
+rf_explain <- explain(rf, data = titanic[ ,-1], predict_function = predict_function,
+                      y = titanic$Survived, label = "Random Forest")
+
+passanger <- titanic[274, -1]
+
+# calculate and plot uncertainty of explanation
+set.seed(123)
+bd <- shap(rf_explain, new_observation = passanger)
+plot(bd)
