bd94f7cb2cbca2007605e91b36f603d15453aa04
jusira <alicjagosiewska@gmail.com>
2021-06-10 18:06:17 +0200

    Update plot

--- benchmark/generate_plots.R
+++ benchmark/generate_plots.R
@@ -9,6 +9,7 @@ auc_plot_data <- auc_df
 num_models <- auc_df$model %>% unique() %>% length()
 auc_plot_data$auc_sort <- rep(auc_plot_data$auc[seq(1,nrow(auc_plot_data), by=num_models)], each = num_models)
 
+
 p <- ggplot(auc_plot_data, aes(x = reorder(factor(task_id), auc_sort), y = auc, color = model)) + 
   geom_point(size = 2) +
   ylim(0,1) +
@@ -34,10 +35,16 @@ i_gbm_id3[1:(nrow(i_gbm_id3)-1), "position"] <- as.numeric(as.character(i_gbm_id
 i_ranger <- ibd_ranger[[obs]][which(ibd_ranger[[obs]]$variable_name != "class"),] 
 i_ranger[1:(nrow(i_ranger)-1), "position"] <- as.numeric(as.character(i_ranger[1:(nrow(i_ranger)-1), "position"])) - 1
 
+i_gbm[,"label"] <- "GBM 1"
+i_gbm_id2[,"label"] <- "GBM 2"
+i_gbm_id3[,"label"] <- "GBM 3"
+i_gbm_id4[,"label"] <- "GBM 4"
+
+
 plot(i_gbm) +
   plot(i_gbm_id2) + 
   plot(i_gbm_id3) + 
-  plot(i_ranger) + 
+  plot(i_ranger)
 
-ggsave("./benchmark/fig/break_down_benchmark.pdf", width = 9, height = 6)
+ggsave("./benchmark/fig/break_down_benchmark.pdf", width = 9, height = 7, scale = 0.85)
 
