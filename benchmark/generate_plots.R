b70835dca6161a9363e66d2331e8a622f913c148
jusira <alicjagosiewska@gmail.com>
2021-06-12 14:18:13 +0200

    Update benchmark performance

--- benchmark/generate_plots.R
+++ benchmark/generate_plots.R
@@ -9,13 +9,22 @@ auc_plot_data <- auc_df
 num_models <- auc_df$model %>% unique() %>% length()
 auc_plot_data$auc_sort <- rep(auc_plot_data$auc[seq(1,nrow(auc_plot_data), by=num_models)], each = num_models)
 
+auc_plot_data$model <- ifelse(auc_plot_data$model == "gbm", "GBM 1", auc_plot_data$model)
+auc_plot_data$model <- ifelse(auc_plot_data$model == "gbm_id2", "GBM 2", auc_plot_data$model)
+auc_plot_data$model <- ifelse(auc_plot_data$model == "gbm_id3", "GBM 3", auc_plot_data$model)
 
-p <- ggplot(auc_plot_data, aes(x = reorder(factor(task_id), auc_sort), y = auc, color = model)) + 
+task_id_order <-  c(9967L,3954L, 9946L, 10093L, 3L, 34537L, 43L, 3899L, 3494L, 3902L, 9978L,
+                    9957L, 3492L, 14965L, 9952L, 37L, 219L, 49L, 3913L, 9983L, 31L,
+                    3917L, 3903L, 3918L, 3493L, 10101L, 9971L, 9980L)
+auc_plot_data$task_id <- factor(auc_plot_data$task_id, levels = rev(task_id_order))
+
+p <- ggplot(auc_plot_data, aes(x = task_id, y = auc, color = model)) + 
   geom_point(size = 2) +
-  ylim(0,1) +
+  scale_y_continuous(limits = c(0.5, 1), expand=c(0,0)) +
   theme_bw() +
   xlab("task id") +
-  theme(axis.text.x = element_text(angle = 90))
+  theme() +
+  coord_flip()
 p
 ggsave("./benchmark/fig/models_performance.pdf", width = 7, height = 3)
 
